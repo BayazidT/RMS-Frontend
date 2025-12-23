778fbaeb240e2bbff4381dba226c196f76f8d311
bayazidt <btnirob13@gmail.com>
2025-12-23 23:57:49 +0100

    schedule continued

--- src/api/employeeApi.ts
+++ src/api/employeeApi.ts
@@ -20,6 +20,10 @@ export const createEmployee = async (data: EmployeeRequest): Promise<Employee> =
   const res = await api.post('/v1/private/users', data);
   return res.data;
 };
+export const getEmployeeById = async (id: string): Promise<Employee> => {
+  const res =  await api.get(`/v1/private/users/${id}`);
+  return res.data;
+}
 
 export const deleteEmployee = async (id: string):Promise<string> => {
   const res = await api.delete<string>(`/v1/private/users/${id}`)
---
+++ src/api/scheduleApi.ts
@@ -0,0 +1,12 @@
+import api from './axiosInstance';
+
+interface GetScheduleParams {
+  page?: number;
+  size?: number;
+  // status?: string;
+}
+
+export const getSchedules = async (string userId): Promise<> =>{
+    
+}
+
